bafba865866538c67e800f4a547fdcef463ccb7d
jpromanonet <programming@jpromano.net>
2021-11-05 19:59:17 -0300

    User ship

--- main.py
+++ main.py
@@ -60,4 +60,13 @@ class Ship:
         self.y = y
         self.health = health
         self.ship_img = None
-        
+        self.laser_img =  None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+        for laser in self.lasers:
+            laser.draw(window)
+
+    def move_lasers(self)
